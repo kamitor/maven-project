bb6eb3617ab2856906148f12408878bd405c1111
kamitor <christiaan_gerardo@hotmail.com>
2017-06-22 11:07:17 +0200

    Update Greeter.java

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -17,6 +17,6 @@ public class Greeter {
    * /
 
   public final String greet (final String someone) {
-    return String.format("Hello, %s!", someone);
+    return String.format("Hello michel, %s!", someone);
   }
 }
